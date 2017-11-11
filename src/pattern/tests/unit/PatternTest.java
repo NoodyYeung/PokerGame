5f04096738434a6c5be0ff60a7f60948caae120c
chatterburger97 <paromac@gmail.com>
2017-11-11 20:36:27 +0800

    PatternTests - initial directory setup and testing procedure

---
+++ src/pattern/tests/unit/PatternTest.java
@@ -0,0 +1,35 @@
+package pattern.tests.unit;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+import org.junit.Assert;
+
+import junit.framework.TestCase;
+import org.junit.Test;
+
+public class PatternTest extends TestCase {
+	
+    @Override
+    public void setUp() throws Exception {
+        super.setUp();
+    }
+
+    
+    // test bomb pattern
+    @Test
+    public void testBombIsSame() {
+    	// TODO
+        boolean result = false;
+        assertEquals(true, result);
+    }
+
+    @Test
+    public void testBombIsLarger() {
+        
+        
+    }
+    
+    
+}
