a42727ef20fa2e2ea66cdc7f26313fe2631d8d16
Man Ka Keung <lllllkaing@gmail.com>
2017-12-11 22:40:22 +0800

    menu test

--- src/test/TestMenu.java
+++ src/test/TestMenu.java
@@ -3,6 +3,7 @@ package test;
 import command.Menu;
 import command.MenuGame;
 import command.MenuMain;
+import command.MenuRoomSelection;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
@@ -39,6 +40,18 @@ public class TestMenu {
                 + "q: Back\r\n", outContent.toString());
     }
 
+    @Test
+    public void testMenuDesc() throws Exception {
+        Menu menuMain = MenuMain.getInstance();
+        String desc = menuMain.description();
+        Assert.assertEquals("Main menu auto loaded", desc);
+    }
+    @Test
+    public void testMenuKey() throws Exception {
+        Menu menuMain = MenuMain.getInstance();
+        String key = menuMain.key();
+        Assert.assertNull(key);
+    }
 
     @Test
     public void testMenuGame() {
@@ -47,4 +60,63 @@ public class TestMenu {
         Assert.assertEquals("p: Play some cards\r\n" +
                 "q: Back\r\n", outContent.toString());
     }
+
+    @Test
+    public void testMenuGameDesc() throws Exception {
+        Menu menuGame = new MenuGame();
+        String desc = menuGame.description();
+        Assert.assertEquals("Begin local game", desc);
+    }
+
+    @Test
+    public void testMenuGameKey() throws Exception {
+        Menu menuGame = new MenuGame();
+        String key = menuGame.key();
+        Assert.assertEquals("2", key);
+    }
+
+    @Test
+    public void testMenuGameExecute() throws Exception {
+        Menu menuGame = new MenuGame();
+        menuGame.execute("1");
+        Assert.assertEquals("Invalid Command [1]!\r\n", outContent.toString());
+    }
+
+    @Test
+    public void testMenuGameExecute2() throws Exception {
+        Menu menuGame = new MenuGame();
+        menuGame.execute("q");
+        Assert.assertEquals("Exit\r\n", outContent.toString());
+    }
+
+    @Test
+    public void testMenuRoomSelection() {
+        Menu menuRoomSelection = new MenuRoomSelection();
+        menuRoomSelection.displayCommandList();
+        Assert.assertEquals("1. Create Room\r\n" +
+                "2. Join Room\r\n" +
+                "3. Disconnect\r\n", outContent.toString());
+    }
+
+    @Test
+    public void testMenuRoomSelectionDesc() throws Exception {
+        Menu menuRoomSelection =new MenuRoomSelection();
+        String desc = menuRoomSelection.description();
+        Assert.assertEquals("Select room or Create room", desc);
+    }
+
+    @Test
+    public void testMenuRoomSelectionKey() throws Exception {
+        MenuRoomSelection menuRoomSelection = new MenuRoomSelection();
+        String key = menuRoomSelection.key();
+        Assert.assertNull(key);
+    }
+
+    @Test
+    public void testMenuRoomSelectionExecute() throws Exception {
+        MenuRoomSelection menuRoomSelection = new MenuRoomSelection();
+        menuRoomSelection.execute();
+        Assert.assertEquals("", outContent.toString());
+    }
+
 }
