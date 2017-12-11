5d06f7a9eeda9cb9cb3567e0f926052c1911a1c6
Yeung Chun Yin <ufofood999@gmail.com>
2017-12-11 22:56:05 +0800

    Test local player

--- src/test/TestPlayer.java
+++ src/test/TestPlayer.java
@@ -100,7 +100,165 @@ public class TestPlayer {
         System.setIn(System.in);
 
     }
-    
+
+
+    @Test
+    public void testLocalPlayerYouTurnToPlayCard2() throws Exception {
+
+
+        Player player = new LocalPlayer();
+        String input = "" + System.lineSeparator()
+                + "D1" + System.lineSeparator() +
+                "DA" + System.lineSeparator();
+        ByteArrayInputStream in = new ByteArrayInputStream(input.getBytes());
+//        System.setIn(in);
+        Main.systemIn = new Scanner(in);
+
+        setOutput();
+        Cards cards = null;
+        ArrayList<Card> c = new ArrayList<>();
+        c.add(new Card("DA"));
+        player.yourTurnToPlayCard(cards, c);
+
+        Assert.assertEquals(
+                "No last player cards. You are free to put cards on the table\n" +
+                        "Card in your hand : ♦DA \n" +
+                        "Please put the cards: (Example: \"D1 S1\")\n" +
+                        "Please put the cards: (Example: \"D1 S1\")\n" +
+                "Invalid Cards Combination\n"+
+                        "Please put the cards: (Example: \"D1 S1\")\n" , getOutput());
+
+        System.setIn(System.in);
+    }
+
+    @Test
+    public void testLocalPlayerYouTurnToPlayCard3() throws Exception {
+
+
+        Player player = new LocalPlayer();
+        String input = "" + System.lineSeparator()
+                + "D1" + System.lineSeparator() +
+                "DA" + System.lineSeparator();
+        ByteArrayInputStream in = new ByteArrayInputStream(input.getBytes());
+//        System.setIn(in);
+        Main.systemIn = new Scanner(in);
+
+        setOutput();
+        ArrayList<Card> c1 = new ArrayList<>();
+        c1.add(new Card("SJ"));
+        Cards cards = new Cards(c1);
+
+        ArrayList<Card> c = new ArrayList<>();
+        c.add(new Card("DA"));
+        player.yourTurnToPlayCard(cards, c);
+
+        Assert.assertEquals(
+                "Last player cards : ♠SJ \n" +
+                        "Card in your hand : ♦DA \n" +
+                        "Please put the cards: (Example: \"D1 S1\")\n" +
+                        "Please put the cards: (Example: \"D1 S1\")\n" +
+                        "Invalid Cards Combination\n"+
+                        "Please put the cards: (Example: \"D1 S1\")\n" , getOutput());
+
+        System.setIn(System.in);
+    }
+
+
+    @Test
+    public void testYourTurnToPlayCardOrSkipCard() throws Exception {
+
+
+        Player player = new LocalPlayer();
+        String input = "" + System.lineSeparator()
+                + "D1" + System.lineSeparator() +
+                "DA" + System.lineSeparator();
+        ByteArrayInputStream in = new ByteArrayInputStream(input.getBytes());
+//        System.setIn(in);
+        Main.systemIn = new Scanner(in);
+
+        setOutput();
+        ArrayList<Card> c1 = new ArrayList<>();
+        c1.add(new Card("SJ"));
+        Cards cards = new Cards(c1);
+
+        ArrayList<Card> c = new ArrayList<>();
+        c.add(new Card("DA"));
+        player.yourTurnToPlayCardOrSkipCard( c, cards);
+
+        Assert.assertEquals(
+                "Last player cards : ♠SJ \n" +
+                        "Card in your hand : ♦DA \n" +
+                        "Please put the cards: (Example: \"D1 S1\") OR input SKIP to skip:\n" +
+                        "Please put the cards: (Example: \"D1 S1\") OR input SKIP to skip:\n" +
+                        "Invalid Cards Combination\n"+
+                        "Please put the cards: (Example: \"D1 S1\") OR input SKIP to skip:\n" , getOutput());
+
+        System.setIn(System.in);
+    }
+
+
+    @Test
+    public void testYourTurnToPlayCardOrSkipCard2() throws Exception {
+
+
+        Player player = new LocalPlayer();
+        String input = "" + System.lineSeparator()
+                + "D1" + System.lineSeparator() +
+                "DA" + System.lineSeparator();
+        ByteArrayInputStream in = new ByteArrayInputStream(input.getBytes());
+//        System.setIn(in);
+        Main.systemIn = new Scanner(in);
+
+        setOutput();
+
+        Cards cards = null;
+
+        ArrayList<Card> c = new ArrayList<>();
+        c.add(new Card("DA"));
+        player.yourTurnToPlayCardOrSkipCard( c, cards);
+
+        Assert.assertEquals(
+                "No last player cards. You are free to put cards on the table\n" +
+                        "Card in your hand : ♦DA \n" +
+                        "Please put the cards: (Example: \"D1 S1\") OR input SKIP to skip:\n" +
+                        "Please put the cards: (Example: \"D1 S1\") OR input SKIP to skip:\n" +
+                        "Invalid Cards Combination\n"+
+                        "Please put the cards: (Example: \"D1 S1\") OR input SKIP to skip:\n" , getOutput());
+
+        System.setIn(System.in);
+    }
+
+
+    @Test
+    public void testYourTurnToPlayCardOrSkipCard3() throws Exception {
+
+
+        Player player = new LocalPlayer();
+        String input = "" + System.lineSeparator()
+                + "SKIP" + System.lineSeparator();
+        ByteArrayInputStream in = new ByteArrayInputStream(input.getBytes());
+//        System.setIn(in);
+        Main.systemIn = new Scanner(in);
+
+        setOutput();
+
+        Cards cards = null;
+
+        ArrayList<Card> c = new ArrayList<>();
+        c.add(new Card("DA"));
+        player.yourTurnToPlayCardOrSkipCard( c, cards);
+
+        Assert.assertEquals(
+                "No last player cards. You are free to put cards on the table\n" +
+                        "Card in your hand : ♦DA \n" +
+                        "Please put the cards: (Example: \"D1 S1\") OR input SKIP to skip:\n" +
+                        "Please put the cards: (Example: \"D1 S1\") OR input SKIP to skip:\n"
+                         , getOutput());
+
+        System.setIn(System.in);
+    }
+
+
     @Test
     public void testLocalPlayerWaitPlayerToPlayerCard() throws Exception {
         Player player = new LocalPlayer();
