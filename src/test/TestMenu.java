dacb3ed8877d449d6b6aa89187fe5dbcbe1e2bac
Man Ka Keung <lllllkaing@gmail.com>
2017-12-11 22:08:12 +0800

    menu

---
+++ src/test/TestMenu.java
@@ -0,0 +1,42 @@
+package test;
+
+import command.Menu;
+import command.MenuMain;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+public class TestMenu {
+
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
+
+    @Before
+    public void setUpStreams() {
+        System.setOut(new PrintStream(outContent));
+        System.setErr(new PrintStream(errContent));
+    }
+
+    @After
+    public void cleanUpStreams() {
+        System.setOut(null);
+        System.setErr(null);
+    }
+
+
+    @Test
+    public void testMenu() throws Exception {
+        Menu menuMain = MenuMain.getInstance();
+        menuMain.displayCommandList();
+        Assert.assertEquals("1: Connect to game\r\n"
+                + "2: Begin local game+\r\n"
+                + "h: Help\r\n"
+                + "q: Back\r\n", outContent.toString());
+    }
+
+
+}
